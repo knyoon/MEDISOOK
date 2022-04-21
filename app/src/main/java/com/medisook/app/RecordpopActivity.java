dc5a419df2c424fd9810c8e7e6d967c70233ffcc
knyoon <kim_na_yoon@naver.com>
2022-04-21 22:26:11 +0900

    ADD: 일단 intent는 성공

---
+++ app/src/main/java/com/medisook/app/RecordpopActivity.java
@@ -0,0 +1,23 @@
+package com.medisook.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecordpopActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.record_pop);
+
+    }
+}
