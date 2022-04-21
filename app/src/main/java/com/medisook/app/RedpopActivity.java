2468002bcdc19e390989ffaf334282dc47374770
YoungKong <youngthing@sookmyung.ac.kr>
2022-04-21 22:40:55 +0900

    ADD. 검색 페이지, 팝업창 구현

---
+++ app/src/main/java/com/medisook/app/RedpopActivity.java
@@ -0,0 +1,24 @@
+package com.medisook.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.view.Window;
+import android.widget.TextView;
+
+
+public class RedpopActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.redpop);
+
+       }
+}
