b3aaf457b9d44882ee3145dcc80c3a68692ffa64
knyoon <kim_na_yoon@naver.com>
2022-04-22 22:47:10 +0900

    ADD: datepicker 시도중

---
+++ app/src/main/java/com/medisook/app/datePickerActivity.java
@@ -0,0 +1,24 @@
+package com.medisook.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.DatePicker;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class datePickerActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.date_picker);
+    }
+}
+
