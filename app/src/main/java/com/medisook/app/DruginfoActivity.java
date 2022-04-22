5bd1e7c57da4e0642948141950ac3b61e5479c06
knyoon <kim_na_yoon@naver.com>
2022-04-22 15:15:31 +0900

    ADD: 팝업화면 띄우기 성공

--- app/src/main/java/com/medisook/app/DruginfoActivity.java
+++ app/src/main/java/com/medisook/app/DruginfoActivity.java
@@ -2,6 +2,7 @@ package com.medisook.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -12,34 +13,30 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class DruginfoActivity extends AppCompatActivity {
+public class DruginfoActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drug_info);
 
-
-        Button record_pop_btn = (Button) findViewById(R.id.record_pop_btn);
-        record_pop_btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(getApplicationContext(), RecordpopActivity.class);
-                startActivity(intent);
-            }
-        });
-
         TextView Medi = findViewById(R.id.medisook); // 변수 선언
         String content = Medi.getText().toString(); //텍스트 가져옴
         SpannableString spannableString = new SpannableString(content); //객체생성
-
         String word = "MEDI";
         int start = content.indexOf(word);
         int end = start + word.length();
-
         spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#68B981")), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-
         Medi.setText(spannableString);
 
+        //버튼
+        final Button record_pop_btn = (Button) findViewById(R.id.record_pop_btn);
+        record_pop_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DruginfoActivity.this, RecordpopActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/medisook/app/RecordpopActivity.java
+++ app/src/main/java/com/medisook/app/RecordpopActivity.java
@@ -2,8 +2,12 @@ package com.medisook.app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.PaintDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,11 +17,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class RecordpopActivity extends Activity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.record_pop);
+        //getWindow().setGravity(Gravity.CENTER);
 
     }
 }
