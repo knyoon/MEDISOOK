89b88ba1cdca0d370a29680beaf5633e2b95be99
YoungKong <youngthing@sookmyung.ac.kr>
2022-04-23 16:37:41 +0900

    ADD. db 연결 test2

---
+++ app/src/main/java/com/medisook/app/CustomDialog.java
@@ -0,0 +1,44 @@
+package com.medisook.app;
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CustomDialog extends Dialog implements View.OnClickListener{
+    private Button okButton;
+    private EditText editText;
+    private Context context;
+    private CustomDialogListener customDialogListener;
+    public CustomDialog(Context context) {
+        super(context);
+        this.context = context;
+    }
+    interface CustomDialogListener{
+        void onOkClicked(String text);
+    }
+    public void setDialogListener(CustomDialogListener customDialogListener){
+        this.customDialogListener = customDialogListener;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.redpop);
+        okButton = (Button) findViewById(R.id.popup_ok_btn);
+        editText = (EditText) findViewById(R.id.editText);
+        okButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.popup_ok_btn:
+                String text = editText.getText().toString();
+                customDialogListener.onOkClicked(text);
+                dismiss();
+                break;
+        }
+    }
+}
