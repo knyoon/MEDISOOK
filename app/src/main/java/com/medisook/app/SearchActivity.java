eadd7b34aff167d1315c1daeb78e8503e381e4ad
YoungKong <youngthing@sookmyung.ac.kr>
2022-04-23 16:35:04 +0900

    ADD. db 연결 test

--- app/src/main/java/com/medisook/app/SearchActivity.java
+++ app/src/main/java/com/medisook/app/SearchActivity.java
@@ -1,5 +1,8 @@
 package com.medisook.app;
-import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
@@ -13,6 +16,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
