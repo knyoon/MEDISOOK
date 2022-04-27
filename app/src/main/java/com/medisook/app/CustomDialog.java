b1fade220d3bae7c6b4fac0be4e5979c93c3c2db
YoungKong <youngthing@sookmyung.ac.kr>
2022-04-27 16:06:30 +0900

    ADD. 220427 리사이클러뷰(메인, 팝업) 구현 완료

--- app/src/main/java/com/medisook/app/CustomDialog.java
+++ app/src/main/java/com/medisook/app/CustomDialog.java
@@ -5,10 +5,16 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class CustomDialog extends AlertDialog implements View.OnClickListener{
+    RecyclerView recyclerView;
+    Adapter_filter adapter;
     private Button okButton;
     private EditText editText;
     private Context context;
@@ -25,12 +31,22 @@ public class CustomDialog extends AlertDialog implements View.OnClickListener{
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE| WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
         getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.redpop);
         okButton = (Button) findViewById(R.id.popup_ok_btn);
-        editText = (EditText) findViewById(R.id.editText);
+        editText = (EditText) findViewById(R.id.search_bar);
         okButton.setOnClickListener(this);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_view_filter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.context, RecyclerView.VERTICAL, false));
+        adapter = new Adapter_filter();
+        for(int i = 0; i<100; i++){
+            String str = i+"번째 필터 컨텐츠";
+            adapter.setArrayData(str);
+        }
+        recyclerView.setAdapter(adapter);
     }
     @Override
     public void onClick(View v) {
