88346914ca3e6e26d32ce2dab060d8b0025e0e6b
knyoon <kim_na_yoon@naver.com>
2022-04-27 19:38:26 +0900

    ADD: fragment로 bottomnavigation 구현

---
+++ app/src/main/java/com/medisook/app/MainMenuActivity.java
@@ -0,0 +1,47 @@
+package com.medisook.app;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainMenuActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private MenuFragmentSearch fragmentSearch = new MenuFragmentSearch();
+    private MenuFragmentMypage fragmentMypage = new MenuFragmentMypage();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.menu_frame_layout, fragmentSearch).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.menu_bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener()
+        {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch (item.getItemId()) {
+                    case R.id.menu_search:
+                        transaction.replace(R.id.menu_frame_layout, fragmentSearch).commitAllowingStateLoss();
+                        break;
+                    case R.id.menu_mypage:
+                        transaction.replace(R.id.menu_frame_layout, fragmentMypage).commitAllowingStateLoss();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    }
---
+++ app/src/main/java/com/medisook/app/MenuFragmentSearch.java
@@ -0,0 +1,22 @@
+package com.medisook.app;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MenuFragmentSearch extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_menu_search, container, false);
+        return rootView;
+        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_menu_main, container, false);
+    }
+}
