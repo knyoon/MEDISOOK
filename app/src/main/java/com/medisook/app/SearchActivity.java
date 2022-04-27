579fb7e54750300a72949bc982076d50ecd98dbf
YoungKong <youngthing@sookmyung.ac.kr>
2022-04-27 18:43:09 +0900

    ADD. 220427 메인페이지 검색 기능 구현 완료

--- app/src/main/java/com/medisook/app/SearchActivity.java
+++ app/src/main/java/com/medisook/app/SearchActivity.java
@@ -1,21 +1,22 @@
 package com.medisook.app;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.TextWatcher;
 import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
@@ -26,7 +27,9 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
     private Button green_filter_btn;
     private Button yellow_filter_btn;
     private TextView txt;
-    private CheckBox checkBox3;
+    ArrayList<DrugItem> drugItemArrayList, filteredList;
+    LinearLayoutManager linearLayoutManager;
+    EditText searchET;
     RecyclerView recyclerView;
     Adapter adapter;
     @Override
@@ -42,15 +45,36 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         int end = start + word.length();
         spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#68B981")), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         Medi.setText(spannableString);
-
+        searchET = findViewById(R.id.search_bar);
         recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+
+        filteredList = new ArrayList<>();
+        drugItemArrayList = new ArrayList<>();
+
+        adapter = new Adapter(drugItemArrayList, this);
+        linearLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
-        adapter = new Adapter();
-        for(int i = 0; i<100; i++){
-            String str = i+"번째 약";
-            adapter.setArrayData(str);
-        }
         recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+        for(int i = 0; i<100; i++){
+            adapter.setArrayData(new DrugItem(i+"번째 약"));
+        }
+        searchET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String searchText = searchET.getText().toString();
+                searchFilter(searchText);
+            }
+        });
+        recyclerView.setAdapter(adapter);
+
+
         red_filter_btn = (Button) findViewById(R.id.red_filter_btn);
         green_filter_btn = (Button) findViewById(R.id.green_filter_btn);
         yellow_filter_btn = (Button) findViewById(R.id.yellow_filter_btn);
@@ -60,6 +84,18 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         yellow_filter_btn.setOnClickListener(this);
         query2();
 
+    }
+    protected void onResume() {
+        super.onResume();
+    }
+    public void searchFilter(String searchText){
+        filteredList.clear();;
+        for (int i=0; i<drugItemArrayList.size(); i++){
+            if(drugItemArrayList.get(i).getDrugName().toLowerCase().contains(searchText.toLowerCase())){
+                filteredList.add(drugItemArrayList.get(i));
+            }
+        }
+        adapter.filterList(filteredList);
     }
     public void query2()
     {
