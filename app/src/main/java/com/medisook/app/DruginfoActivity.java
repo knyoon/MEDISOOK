7ca222bf263a54103850a4ffe3daa736c07779b8
knyoon <kim_na_yoon@naver.com>
2022-04-30 15:10:02 +0900

    ADD: (error있음) druginfo 까지 연결 되는데 fragment로 처리 안됨.

--- app/src/main/java/com/medisook/app/DruginfoActivity.java
+++ app/src/main/java/com/medisook/app/DruginfoActivity.java
@@ -39,15 +39,16 @@ public class DruginfoActivity extends AppCompatActivity implements View.OnClickL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drug_info);
 
-        TextView Medi = findViewById(R.id.medisook); // 변수 선언
-        String content = Medi.getText().toString(); //텍스트 가져옴
-        SpannableString spannableString = new SpannableString(content); //객체생성
-        String word = "MEDI";
-        int start = content.indexOf(word);
-        int end = start + word.length();
-        spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#68B981")), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        Medi.setText(spannableString);
+//        TextView Medi = findViewById(R.id.medisook); // 변수 선언
+//        String content = Medi.getText().toString(); //텍스트 가져옴
+//        SpannableString spannableString = new SpannableString(content); //객체생성
+//        String word = "MEDI";
+//        int start = content.indexOf(word);
+//        int end = start + word.length();
+//        spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#68B981")), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+//        Medi.setText(spannableString);
         //버튼
+
         final Button record_pop_btn = (Button) findViewById(R.id.record_pop_btn);
         record_pop_btn.setOnClickListener(this);
 
