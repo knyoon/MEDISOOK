29c9224e05929c664959a9508d5ab103cc69671a
knyoon <kim_na_yoon@naver.com>
2022-05-07 00:11:24 +0900

    ADD: druginfo 페이지도 fragment로 구현!

--- app/src/main/java/com/medisook/app/DruginfoActivity.java
+++ app/src/main/java/com/medisook/app/DruginfoActivity.java
@@ -1,6 +1,7 @@
 package com.medisook.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
@@ -14,6 +15,7 @@ import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -23,7 +25,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class DruginfoActivity extends AppCompatActivity implements View.OnClickListener{
+public class DruginfoActivity extends Fragment implements View.OnClickListener{
 //    Calendar myCalendar = Calendar.getInstance();
 //    DatePickerDialog.OnDateSetListener myDatePicker = new DatePickerDialog.OnDateSetListener() {
 //        @Override
@@ -35,10 +37,9 @@ public class DruginfoActivity extends AppCompatActivity implements View.OnClickL
 //        }
 //    };
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drug_info);
-
 //        TextView Medi = findViewById(R.id.medisook); // 변수 선언
 //        String content = Medi.getText().toString(); //텍스트 가져옴
 //        SpannableString spannableString = new SpannableString(content); //객체생성
@@ -49,17 +50,22 @@ public class DruginfoActivity extends AppCompatActivity implements View.OnClickL
 //        Medi.setText(spannableString);
         //버튼
 
-        final Button record_pop_btn = (Button) findViewById(R.id.record_pop_btn);
-        record_pop_btn.setOnClickListener(this);
+//        final Button record_pop_btn = (Button) findViewById(R.id.record_pop_btn);
+//        record_pop_btn.setOnClickListener(this);
+//        return rootView;
 
     }
+
+    public void setContentView(int drug_info) {
+    }
+
     @Override
     public void onClick(View v) {
-        //LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LayoutInflater inflater = (LayoutInflater) this.getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         switch (v.getId()){
             case R.id.record_pop_btn:
-                CustomDialog_record dialog = new CustomDialog_record(this);
-                CustomDialog_record.Builder dialog_bulider = new CustomDialog_record.Builder(this);
+                CustomDialog_record dialog = new CustomDialog_record(getActivity());
+                CustomDialog_record.Builder dialog_bulider = new CustomDialog_record.Builder(getActivity());
                 dialog.setDialogListener(new CustomDialog_record.CustomDialogListener() {
                     @Override
                     public void onOkClicked(String text) {
@@ -71,6 +77,17 @@ public class DruginfoActivity extends AppCompatActivity implements View.OnClickL
 //                new DatePickerDialog(this, myDatePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
 //                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
         }
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.drug_info, container, false);
+        //textview = (TextView) rootView.findViewById(R.id.medisook);
+        final Button record_pop_btn = (Button) rootView.findViewById(R.id.record_pop_btn);
+        record_pop_btn.setOnClickListener(this);
+        return rootView;
+
+        //return inflater.inflate(R.layout.fragment_menu_mypage, container, false);
     }
 //    private void updateLabel(){
 //        String myFormat = "yyyy/MM/dd";
