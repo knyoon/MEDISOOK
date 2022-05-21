8c8f0d61e6c901a6a8650bca5b192be9d3db155b
82103 <seyoungkim1715231>
2022-05-21 14:06:50 +0900

    아ㅣㄴ왜자꾸지워지는데

--- app/src/main/java/com/medisook/app/MenuFragmentSearch.java
+++ app/src/main/java/com/medisook/app/MenuFragmentSearch.java
@@ -1,18 +1,9 @@
 package com.medisook.app;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.text.Editable;
-import android.text.SpannableString;
-import android.text.Spanned;
 import android.text.TextWatcher;
-import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +11,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
