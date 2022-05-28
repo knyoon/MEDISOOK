67cbf7d645a36c7f4848e8a8365d0ee8cc1d828b
YoungKong <youngthing@sookmyung.ac.kr>
2022-05-28 14:17:15 +0900

    ADD. 220528 fragment로 이름, 이미지 넘기기

--- app/src/main/java/com/medisook/app/DruginfoActivity.java
+++ app/src/main/java/com/medisook/app/DruginfoActivity.java
@@ -1,7 +1,9 @@
 package com.medisook.app;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,10 +11,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 public class DruginfoActivity extends Fragment implements View.OnClickListener{
+    TextView drugName_view;
+    ImageView drugImg_view;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,12 +48,30 @@ public class DruginfoActivity extends Fragment implements View.OnClickListener{
     }
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.drug_info, container, false);
+        drugName_view = (TextView) rootView.findViewById(R.id.drugName);
+        drugImg_view = (ImageView) rootView.findViewById(R.id.drugImage);
         Bundle bundle = getArguments();
         int position = bundle.getInt("position");
         ArrayList<DrugItem> drugItem = (ArrayList<DrugItem>) bundle.getSerializable(("DrugItem"));
+        String drugName = drugItem.get(position).getDrugName();
+        String drugImage  = drugItem.get(position).getDrugImg();
+        if(drugImage.isEmpty()==false){
+            Picasso.get()
+                    .load(drugImage)
+                    .error(R.drawable.drung_sampleimage)
+                    .placeholder(R.drawable.drung_sampleimage)
+                    .into(drugImg_view);
+        }
+        else{
+            Drawable drawable= ContextCompat.getDrawable(getContext(), R.drawable.drung_sampleimage);
+            drugImg_view.setImageDrawable(drawable);
+        }
         Log.d("test", "position : " + position);
         Log.d("test", "drugitem : " + drugItem.size());
         Log.d("test", "drugitem : " + drugItem.get(0));
+        drugName_view.setText(drugName);
+
+        
         final Button record_pop_btn = (Button) rootView.findViewById(R.id.record_pop_btn);
         record_pop_btn.setOnClickListener(this);
         return rootView;
