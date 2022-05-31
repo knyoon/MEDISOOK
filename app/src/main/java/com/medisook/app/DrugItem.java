561cd898ce8282b852fd69d0c02cdda8b43db7f2
YoungKong <youngthing@sookmyung.ac.kr>
2022-05-31 17:42:56 +0900

    ADD. 220531 drug info

--- app/src/main/java/com/medisook/app/DrugItem.java
+++ app/src/main/java/com/medisook/app/DrugItem.java
@@ -5,41 +5,155 @@ import java.io.Serializable;
 public class DrugItem implements Serializable {
     String drugName;
     String drugImg;
+    String drugentp;
+    String drugcode;
+    String qnt;
+    String otc;
+    String chart;
+    String efcy;
+    String classname;
+    String usemethod;
+    String qesitm;
+    String term;
+    String deposit;
+    String totalcontent;
+    String mainingr;
+    String ingrname;
 
-//        private String member_name;
-//        private String member_entp;
-//        private String member_image;
-//        public String getMember_id() {
-//            return member_name;
-//        }
-//        public String getMember_name() {
-//            return member_entp;
-//        }
-//        public String getMember_image() {
-//            return member_image;
-//        }
-//        public void setMember_id(String member_id) {
-//            this.member_name = member_id;
-//        }
-//        public void setMember_name(String member_name) {
-//            this.member_entp = member_name;
-//        }
-//        public void setMember_image(String member_address) {
-//            this.member_image = member_image;
-//        }
-    public DrugItem(){}
-    public DrugItem(String drugName){}
-        public String getDrugName(){
-            return drugName;
-        }
-        public void setDrugName(String drugName){
-            this.drugName = drugName;
-        }
-        public String getDrugImg(){
-            return drugImg;
-        }
-        public void setDrugImg(String drugImg){
-            this.drugImg = drugImg;
-        }
+    public DrugItem() {
     }
+
+    public DrugItem(String drugName) {
+    }
+
+    public String getDrugName() {
+        return drugName;
+    }
+
+    public void setDrugName(String drugName) {
+        this.drugName = drugName;
+    }
+
+    public String getDrugImg() {
+        return drugImg;
+    }
+
+    public void setDrugImg(String drugImg) {
+        this.drugImg = drugImg;
+    }
+
+    public String getDrugentp() {
+        return drugentp;
+    }
+
+    public void setDrugentp(String drugentp) {
+        this.drugentp = drugentp;
+    }
+
+    public String getDrugcode() {
+        return drugcode;
+    }
+
+    public void setDrugcode(String drugcode) {
+        this.drugcode = drugcode;
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public void setClassname(String classname) {
+        this.classname = classname;
+    }
+
+    public String getQnt() {
+        return qnt;
+    }
+
+    public void setQnt(String qnt) {
+        this.qnt = qnt;
+    }
+
+    public String getOtc() {
+        return otc;
+    }
+
+    public void setOtc(String otc) {
+        this.otc = otc;
+    }
+
+    public String getChart() {
+        return chart;
+    }
+
+    public void setChart(String chart) {
+        this.chart = chart;
+    }
+
+    public String getEfcy() {
+        return efcy;
+    }
+
+    public void setEfcy(String efcy) {
+        this.efcy = efcy;
+    }
+
+    public String getUsemethod() {
+        return usemethod;
+    }
+
+    public void setUsemethod(String usemethod) {
+        this.usemethod = usemethod;
+    }
+
+    public String getQesitm() {
+        return qesitm;
+    }
+
+    public void setQesitm(String qesitm) {
+        this.qesitm = qesitm;
+    }
+
+    public String getDeposit() {
+        return deposit;
+    }
+
+    public void setDeposit(String deposit) {
+        this.deposit = deposit;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public String getTotalcontent() {
+        return totalcontent;
+    }
+
+    public void setTotalcontent(String totalcontent) {
+        this.totalcontent = totalcontent;
+    }
+
+    public String getMainingr() {
+        return mainingr;
+    }
+
+    public void setMainingr(String mainingr) {
+        this.mainingr = mainingr;
+    }
+
+    public String getIngrname() {
+        return ingrname;
+    }
+
+    public void setIngrname(String ingrname) {
+        this.ingrname = ingrname;
+    }
+
+
+}
 
