762c66497448bc6db7a03eab29318a03039cbd8e
knyoon <kim_na_yoon@naver.com>
2022-06-12 16:05:19 +0900

    ADD: 220612 회원가입 기능 완료!

--- app/src/main/java/com/medisook/app/JoinActivity.java
+++ app/src/main/java/com/medisook/app/JoinActivity.java
@@ -1,54 +1,92 @@
 package com.medisook.app;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.regex.Pattern;
+
 public class JoinActivity extends AppCompatActivity implements View.OnClickListener {
-    private EditText nickname;
-    private EditText password;
+    private EditText et_nickname;
+    private EditText et_password;
+    private EditText warn_nk;
+    private EditText warn_pw;
+    private Button join_btn;
     private Context mContext;
+    boolean nk_result=false;
+    boolean pw_result = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join);
 
-        nickname = (EditText) findViewById(R.id.nickname);
-        nickname.setOnKeyListener(new View.OnKeyListener(){
+        warn_nk = (EditText) findViewById(R.id.warn_nk);
+        warn_pw = (EditText) findViewById(R.id.warn_pw);
+        et_nickname = (EditText) findViewById(R.id.nickname);
+        join_btn = (Button) findViewById(R.id.join_btn);
+        join_btn.setOnClickListener(this);
+
+        et_nickname.setOnClickListener(this);
+        et_nickname.setOnKeyListener(new View.OnKeyListener(){
             @Override
             public boolean onKey(View view, int i, KeyEvent keyEvent) {
-                String nk = nickname.getText().toString();
+                String nk =  et_nickname.getText().toString();
                 switch (i){
                     case KeyEvent.KEYCODE_ENTER:
                         if (keyEvent.getAction() == keyEvent.ACTION_UP) {
+                            if(nk.length() >= 11 || nk.length()==0) {
+                                et_nickname.setText(null);
+                                warn_nk.setText("!잘못된 형식입니다.");
+                                nk_result= false;
+                            }
+                            else{
+                                warn_nk.setText(null);
+                                nk_result= true;
+                            }
+                            Log.d("회원가입", "result: " + nk_result);
                             Log.d("회원가입", "닉네임: " + nk);
                             InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                             //getWindow().sethideSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
                             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-                        } return true;
+                        }
+                        return true;
                     case KeyEvent.KEYCODE_DEL:
                 }
                 return false;
             }
         });
 
-        password = (EditText) findViewById(R.id.password);
-        password.setOnKeyListener(new View.OnKeyListener(){
+        et_password = (EditText) findViewById(R.id.password);
+        et_password.setOnClickListener(this);
+        et_password.setOnKeyListener(new View.OnKeyListener(){
             @Override
             public boolean onKey(View view, int i, KeyEvent keyEvent) {
-                String pw = password.getText().toString();
+                String pw = et_password.getText().toString();
                 switch (i){
                     case KeyEvent.KEYCODE_ENTER:
                         if (keyEvent.getAction() == keyEvent.ACTION_UP) {
+                            if(Pattern.matches("^[A-Za-z0-9]{8,12}$", pw)) {
+                                warn_pw.setText(null);
+                                pw_result = true;
+                            }
+                            else {
+                                et_password.setText(null);
+                                warn_pw.setText("!잘못된 형식입니다.");
+                                pw_result = false;
+                            }
+                            Log.d("회원가입", "패스워드: " + pw_result);
                             Log.d("회원가입","패스워드: " + pw);
                             InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
@@ -58,28 +96,26 @@ public class JoinActivity extends AppCompatActivity implements View.OnClickListe
                 return false;
             }
         });
-
     }
 
     @Override
     public void onClick(View v) {
-
-        boolean result=false;
-        String nk = nickname.getText().toString();
-        Log.v("회원가입", nk + " : 입력한 닉네임");
-        switch (v.getId()) {
-            case R.id.nickname:
-                if(nk.length()>10){
-                    Toast.makeText(JoinActivity.this, "닉네임은 10글자 이하로 입력해주세요",
-                            Toast.LENGTH_LONG).show();
-                    result= false;
+        //Log.v("회원가입", "테스트: " + nk_result);
+        //Log.v("회원가입", "테스트: " + pw_result);
+        switch (v.getId()){
+            case R.id.join_btn:
+                Log.v("회원가입", "테스트- " + nk_result + pw_result);
+                if(nk_result == true | pw_result == true){
+                    Toast.makeText(this.getApplicationContext(),"회원가입 성공!", Toast.LENGTH_SHORT).show();
+                    Intent intentMainActivity =
+                            new Intent(JoinActivity.this, LoginActivity.class);
+                    startActivity(intentMainActivity);
                 }
-                else {
-                    result=true;
+                else{
+                    Toast.makeText(this.getApplicationContext(),"회원가입을 실패했습니다.\n다시 시도하십시오.", Toast.LENGTH_SHORT).show();
+                    et_password.setText(null);
+                    et_nickname.setText(null);
                 }
-
-//            case R.id.password:
-
         }
     }
 }
