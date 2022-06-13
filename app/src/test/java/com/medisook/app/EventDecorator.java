a6362146f81cb14100a925c3247917fb9eda7f07
knyoon <kim_na_yoon@naver.com>
2022-06-13 21:41:05 +0900

    ADD: 220613 eventdecorator

---
+++ app/src/test/java/com/medisook/app/EventDecorator.java
@@ -0,0 +1,10 @@
+package com.medisook.app;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+
+import java.util.List;
+
+public class EventDecorator {
+    public EventDecorator(int green, List<CalendarDay> calendarDays, CustomDialog_calender customDialog_calender) {
+    }
+}
