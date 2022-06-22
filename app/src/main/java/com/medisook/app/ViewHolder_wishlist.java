3d51f46caf500418bcb18da698438d23deaf05cc
82103 <seyoungkim1715231>
2022-06-22 13:46:12 +0900

    기록하기 태그 하나만 해도됨

---
+++ app/src/main/java/com/medisook/app/ViewHolder_wishlist.java
@@ -0,0 +1,38 @@
+package com.medisook.app;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolder_wishlist extends RecyclerView.ViewHolder {
+    public TextView txt;
+    ViewHolder_wishlist(View itemView, final OnItemClickListener listener){
+        super(itemView);
+        txt = (TextView) itemView.findViewById(R.id.wishlist_content);
+        itemView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+//        itemView.setOnLongClickListener(new View.OnLongClickListener(){
+//            @Override
+//            public boolean onLongClick(View view) {
+//                int position = getAbsoluteAdapterPosition();
+//                if(position != RecyclerView.NO_POSITION){
+//
+//                }
+//                return false;
+//            }
+//        });
+
+//        txt.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+////                String strText = txt.getText().toString();
+////                Toast.makeText(context, strText, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+    }
+}
